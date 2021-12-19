fec27ff5c50663bb17b58dc16083a8d7f1321542
IslamAhmed1092 <eslam1092@hotmail.com>
2021-12-19 18:47:05 +0200

    add binarization using otsu

--- preprocessing.py
+++ preprocessing.py
@@ -8,7 +8,12 @@ def load_data():
     y = []
     for classNum in range(1, 10):
         for filename in sorted(glob.glob(f'ACdata_base/{classNum}/*.jpg')):
-            img = cv2.imread(filename)
+            img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
             x.append(img)
             y.append(classNum)
     return np.asarray(x, dtype=object), np.asarray(y, dtype=object)
+
+def binraization(img):
+    blur = cv2.GaussianBlur(img,(3,3),0)
+    _, binaryImage = cv2.threshold(blur,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return binaryImage
